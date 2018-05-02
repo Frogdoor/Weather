4bd6938e2098ea807a34ac09731f850a192cd93a
Marcel <armstrong091@gmail.com>
2018-05-02 20:34:00 -0300

    Cleaned up the code. added some comments.

--- NewORTest.py
+++ NewORTest.py
@@ -2,65 +2,89 @@ import pickle
 from BRAIN_BUILDER import *
 
 
+#---Variables------------------------------------
+NumLayers	= 3	#Number of hidden layers
+NumNodes 	= 4	#Number of nodes per hidden layer
+NumInputs	= 1 #Number of Input nodes
+NumOutputs	= 1	#Number of Ouput nodes
+
+cntr = 0 #Training Itteration counter
+
+LearnRate = .25 #Backpropation learning rate
+
+tol=0.1 #tolerance on answer
+
+nCores = 1 #how many cores belong to the brain
+#-----------------------------------------------
+
+
+#Create the Brain
+theBrain = RandomBrain(NumLayers,NumNodes,NumInputs,NumOutputs)
+
+#start data logging
+f = open("LearningCurve.dat", 'a')
+f.write("\n\n")
+
+
 def randomCoin(): # flip a coin return 1 or 0
 	x= 1 if random.random() < 0.5 else 0
 	return x
 	
 	
-def GenTrainSet(NumInputs):
-	Ins=[]
-	for i in range(NumInputs) :
-		Ins.append(randomCoin())
-		
-	Ans = any(Ins)
+def TraningFunction():
 	
+	Ins=[]
+	for i in range(NumInputs):
+		Ins.append(random.random())
+	
+	Ans =  pow(Ins[0],2)
+
+#	And/OR function
+#	Ins=[]
+#	for i in range(NumInputs) :
+#		Ins.append(randomCoin())
+#		
+#	Ans = all(Ins)
+#	
 	return Ins,Ans
 
 
-#RandomBrain(NumLayers,numNodes,numInputs,numOutputs)
-theBrain = RandomBrain(1,4,2,1)
-
-
-
-tol=0.15 #tolerance on answer
-
-cntr=0
-cntr2=0
-
+#Training loop
 while True:
-
-
-	inputs,answer = GenTrainSet(2)
+	
+	#Get some traning Data
+	inputs,answer = TraningFunction()
 
 	#Run the Brain
-	RunBrain(inputs,theBrain,1)
+	RunBrain(inputs,theBrain,nCores)
 	
 	guess = round(theBrain[len(theBrain)-1][0].Value,2)
 	
-	#print "Wanted: " +str(answer)+"\tGot: "+str(guess)
 	
 	#Train the brain
-	BackProp(theBrain,[answer],1.)
+	BackProp(theBrain,[answer],LearnRate) #Brain, Key, rate
 	cntr += 1
 
 
-	#print "------TEST!!!!-------"
+	#Test the brain
 	test=[]
 	for i in range(100):
-		inputs,answer = GenTrainSet(2)
+		inputs,answer = TraningFunction()
 		
-		RunBrain(inputs,theBrain,1)
+		RunBrain(inputs,theBrain,nCores)
 		guess = round(theBrain[len(theBrain)-1][0].Value,2)
 		
-		
-		#print guess,answer
 		test.append((abs(guess-answer)<tol))
+		
+	f.write(str(cntr)+" " + str(sum(test)) + "\n")
 		
 	if cntr%10 == 0 : print "Gen " + str(cntr) +" - " "Score: " + str(sum(test)) + "/" + str(len(test)) 
 	if all(test): break
 	
 print "Brain Trained! It took "+str(cntr)+" training sessions."
 
+
+f.close()
 
 while True:
 
@@ -70,9 +94,9 @@ while True:
 
 	test=[]
 	for i in range(20):
-		inputs,answer = GenTrainSet(2)
+		inputs,answer = TraningFunction()
 		
-		RunBrain(inputs,theBrain,1)
+		RunBrain(inputs,theBrain,nCores)
 		guess = round(theBrain[len(theBrain)-1][0].Value,2)
 		
 		test.append((abs(guess-answer)<tol))
